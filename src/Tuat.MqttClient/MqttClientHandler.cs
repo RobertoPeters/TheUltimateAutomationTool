da38ece9f145da8fc59f0fa0c6d7e59b34d1e2d3
Robert Peters <RobertPeters@camasit.nl>
2025-11-21 11:01:47 +0100

    added mqtt client

---
+++ src/Tuat.MqttClient/MqttClientHandler.cs
@@ -0,0 +1,382 @@
+﻿using MQTTnet;
+using System.Collections.Concurrent;
+using System.ComponentModel;
+using Tuat.Interfaces;
+using Tuat.Models;
+
+namespace Tuat.MqttClient;
+
+[DisplayName("MQTT")]
+[Editor("Tuat.MqttClient.ClientSettings", typeof(ClientSettings))]
+public class MqttClientHandler(Client _client, IVariableService _variableService, IMessageBusService _messageBusService) : IClientHandler, IClientConnected
+{
+    public class ClientProperties
+    {
+        public string? Host { get; set; }
+        public bool Tls { get; set; }
+        public bool WebSocket { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+    }
+
+    private ClientProperties _clientProperties = new();
+    private IMqttClient? _mqttClient;
+    private Timer? _reconnectTimer = null;
+    private readonly ConcurrentDictionary<string, List<VariableInfo>> _variables = [];
+
+    public Client Client => _client;
+
+    public bool IsConnected => _mqttClient?.IsConnected ?? false;
+
+    public List<(string methodName, bool isAutomationVariable, bool persistant, string description, string example)> CreateVariableOnClientMethods() => [];
+
+    public List<(string methodName, string command, string description, string example)> CreateExecuteOnClientMethods() =>
+        [
+            ("mqttPublish", "publish", "Publish a MQTT variable.", """mqttPublish('mqttnet/samples/topic/2', 'on')""")
+        ];
+
+    public async Task AddOrUpdateVariableInfoAsync(List<VariableInfo> variables)
+    {
+        foreach (var variable in variables)
+        {
+            var existingVariable = _variables.Values.SelectMany(x => x)
+                .Where(x => x.Variable.Id == variable.Variable.Id)
+                .Select(x => x)
+                .FirstOrDefault();
+
+            if (existingVariable != null)
+            {
+                await UpdateVariableInfoAsync(variable);
+            }
+            else if (!string.IsNullOrWhiteSpace(variable.Variable.Data))
+            {
+                await AddVariableInfoAsync(variable);
+            }
+        }
+    }
+
+    private async Task AddVariableInfoAsync(VariableInfo variable)
+    {
+        if (!_variables.TryGetValue(variable.Variable.Data!, out var variableList))
+        {
+            variableList = [];
+            _variables.TryAdd(variable.Variable.Data!, variableList);
+        }
+        variableList.Add(variable);
+        if (variableList.Count == 1)
+        {
+            if (_mqttClient != null && _mqttClient?.IsConnected == true)
+            {
+                try
+                {
+                    var options = new MqttClientSubscribeOptions()
+                    {
+                        TopicFilters = [new MQTTnet.Packets.MqttTopicFilter() { Topic = variable.Variable.Data }]
+                    };
+                    await _mqttClient!.SubscribeAsync(options);
+                }
+                catch
+                {
+                    //nothing
+                }
+            }
+        }
+    }
+
+    private async Task UpdateVariableInfoAsync(VariableInfo variable)
+    {
+        if (variable.Variable.PreviousData != variable.Variable.Data)
+        {
+            await DeleteVariableInfoAsync(variable, variable.Variable.PreviousData);
+            if (!string.IsNullOrWhiteSpace(variable.Variable.Data))
+            {
+                await AddVariableInfoAsync(variable);
+            }
+        }
+    }
+
+    public async Task DeleteVariableInfoAsync(List<VariableInfo> variables)
+    {
+        foreach (var variable in variables)
+        {
+            await DeleteVariableInfoAsync(variable, variable.Variable.Data);
+        }
+    }
+
+    private async Task DeleteVariableInfoAsync(VariableInfo variable, string? topic)
+    {
+        if (!string.IsNullOrWhiteSpace(topic) && _variables.TryGetValue(topic, out var variableList))
+        {
+            var variableInList = variableList.FirstOrDefault(x => x.Variable.Id == Math.Abs(variable.Variable.Id));
+            if (variableInList != null)
+            {
+                variableList.Remove(variableInList);
+                if (!variableList.Any())
+                {
+                    _variables.TryRemove(topic, out _);
+                    if (_mqttClient != null && _mqttClient.IsConnected)
+                    {
+                        try
+                        {
+                            var options = new MqttClientUnsubscribeOptions()
+                            {
+                                TopicFilters = [topic]
+                            };
+                            await _mqttClient.UnsubscribeAsync(options);
+                        }
+                        catch
+                        {
+                            //nothing
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public ValueTask DisposeAsync()
+    {
+        DisposeMqttClient();
+        return ValueTask.CompletedTask;
+    }
+
+    public async Task<bool> ExecuteAsync(int? variableId, string command, object? parameter1, object? parameter2, object? parameter3)
+    {
+        var existingVariable = variableId == null ? null : _variables.Values.SelectMany(x => x)
+              .Where(x => x.Variable.Id == variableId)
+              .Select(x => x)
+              .FirstOrDefault();
+
+        if (variableId != null && existingVariable == null)
+        {
+            return false;
+        }
+
+        var result = false;
+        switch (command.ToLower())
+        {
+            case "publish":
+                if (parameter1 != null && parameter2 != null)
+                {
+                    result = await PublishAsync(parameter1.ToString()!, parameter2.ToString()!);
+                }
+                break;
+        }
+        return result;
+    }
+
+    public async Task StartAsync()
+    {
+        DisposeMqttClient();
+        _variables.Clear();
+        var variables = _variableService.GetVariables()
+          .Where(x => x.Variable.ClientId == _client.Id);
+        foreach (var variable in variables)
+        {
+            if (string.IsNullOrWhiteSpace(variable.Variable.Data))
+            {
+                continue;
+            }
+
+            if (!_variables.TryGetValue(variable.Variable.Data, out var list))
+            {
+                list = new List<VariableInfo>();
+                _variables.TryAdd(variable.Variable.Data, list);
+            }
+            list.Add(variable);
+        }
+
+        if (!string.IsNullOrWhiteSpace(_client.Settings))
+        {
+            _clientProperties = System.Text.Json.JsonSerializer.Deserialize<ClientProperties>(_client.Settings) ?? new();
+        }
+        await CreateMqttClientAsync();
+    }
+
+    public async Task UpdateAsync(Client client)
+    {
+        DisposeMqttClient();
+        _client = client;
+        if (!string.IsNullOrWhiteSpace(client.Settings))
+        {
+            _clientProperties = System.Text.Json.JsonSerializer.Deserialize<ClientProperties>(client.Settings) ?? new();
+        }
+        else
+        {
+            _clientProperties = new();
+        }
+        await CreateMqttClientAsync();
+    }
+
+    private async Task CreateMqttClientAsync()
+    {
+        var mqttFactory = new MqttClientFactory();
+        _mqttClient = mqttFactory.CreateMqttClient();
+        _mqttClient.ApplicationMessageReceivedAsync += OnMessageReceived;
+        if (_client.Enabled && !string.IsNullOrWhiteSpace(_clientProperties.Host))
+        {
+            await CheckConnectionAsync();
+        }
+    }
+
+    private async Task CheckConnectionAsync()
+    {
+        if (_mqttClient != null)
+        {
+            bool oldState = _mqttClient.IsConnected;
+            try
+            {
+                if (!await _mqttClient.TryPingAsync())
+                {
+                    //try connect
+                    var parts = _clientProperties.Host!.Split(':', 2);
+                    var mqttClientOptionsPreBuild = new MqttClientOptionsBuilder();
+
+                    if (parts.Length == 1)
+                    {
+                        mqttClientOptionsPreBuild = mqttClientOptionsPreBuild.WithTcpServer(_clientProperties.Host);
+                    }
+                    else
+                    {
+                        mqttClientOptionsPreBuild = mqttClientOptionsPreBuild.WithTcpServer(parts[0], int.Parse(parts[1]));
+                    }
+
+                    if (_clientProperties.Tls)
+                    {
+                        var tlsOptions = new MqttClientTlsOptions()
+                        {
+                            UseTls = true
+                        };
+                        mqttClientOptionsPreBuild = mqttClientOptionsPreBuild.WithTlsOptions(tlsOptions);
+                    }
+
+                    if (!string.IsNullOrWhiteSpace(_clientProperties.Username))
+                    {
+                        mqttClientOptionsPreBuild = mqttClientOptionsPreBuild.WithCredentials(_clientProperties.Username, _clientProperties.Password);
+                    }
+
+                    var mqttClientOptions = mqttClientOptionsPreBuild.Build();
+                    using (var timeoutToken = new CancellationTokenSource(TimeSpan.FromSeconds(10)))
+                    {
+                        await _mqttClient.ConnectAsync(mqttClientOptions, timeoutToken.Token);
+
+                        if (_variables.Any())
+                        {
+                            var options = new MqttClientSubscribeOptions()
+                            {
+                                TopicFilters = _variables.Keys.Select(x => new MQTTnet.Packets.MqttTopicFilter() { Topic = x }).ToList()
+                            };
+                            await _mqttClient!.SubscribeAsync(options);
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                //none
+            }
+            _reconnectTimer = new Timer(async (state) =>
+            {
+                await CheckConnectionAsync();
+            }, null, 5000, Timeout.Infinite);
+
+            if (_mqttClient != null && oldState != _mqttClient.IsConnected)
+            {
+                await _messageBusService.PublishAsync(new ClientConnectionInfo()
+                {
+                    ClientId = _client.Id,
+                    IsConnected = _mqttClient.IsConnected,
+                });
+            }
+        }
+    }
+
+    private void DisposeMqttClient()
+    {
+        _reconnectTimer?.Dispose();
+        _reconnectTimer = null;
+        if (_mqttClient != null)
+        {
+            _mqttClient.ApplicationMessageReceivedAsync -= OnMessageReceived;
+            try
+            {
+                _mqttClient.Dispose();
+            }
+            catch
+            {
+                //nothing
+            }
+            _mqttClient = null;
+        }
+    }
+
+    private async Task OnMessageReceived(MqttApplicationMessageReceivedEventArgs args)
+    {
+        var topic = args.ApplicationMessage?.Topic;
+        if (!string.IsNullOrWhiteSpace(topic))
+        {
+            switch (topic)
+            {
+                case "$SYS/broker/publish/messages/received":
+                case "$SYS/broker/publish/messages/sent":
+                case "$SYS/broker/messages/sent":
+                case "$SYS/broker/messages/received":
+                case "$SYS/broker/publish/bytes/received":
+                case "$SYS/broker/publish/bytes/sent":
+                case "$SYS/broker/bytes/sent":
+                case "$SYS/broker/bytes/received":
+                    break;
+                default:
+                    try
+                    {
+                        var payload = args.ApplicationMessage.ConvertPayloadToString();
+
+                        await UpdateVariableValue(topic, payload);
+                    }
+                    catch
+                    {
+                        //yep
+                    }
+                    break;
+            }
+        }
+    }
+
+    private async Task UpdateVariableValue(string topic, string? payload)
+    {
+        if (_variables.TryGetValue(topic, out var variableList))
+        {
+            if (variableList.Any())
+            {
+                await _variableService.SetVariableValuesAsync(variableList.Select(x => (variableId: x.Variable.Id, value: payload)).ToList());
+            }
+        }
+    }
+
+    private async Task<bool> PublishAsync(string topic, string? data)
+    {
+        if (!_mqttClient?.IsConnected == true)
+        {
+            return false;
+        }
+
+        var msgBuilder = new MqttApplicationMessageBuilder()
+            .WithTopic(topic);
+
+        //in future more options than string?
+        msgBuilder = msgBuilder.WithPayload(data);
+
+        var msg = msgBuilder.Build();
+
+        try
+        {
+            var result = await _mqttClient!.PublishAsync(msg, CancellationToken.None);
+            return result.IsSuccess;
+        }
+        catch
+        {
+
+            return false;
+        }
+    }
+}
