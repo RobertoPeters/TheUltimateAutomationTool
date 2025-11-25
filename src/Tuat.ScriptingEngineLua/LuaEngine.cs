70be95ff770391115ad118999020bda1d291dcc7
Robert Peters <RobertPeters@camasit.nl>
2025-11-25 08:47:34 +0100

    Lua support

--- src/Tuat.ScriptingEngineLua/LuaEngine.cs
+++ src/Tuat.ScriptingEngineLua/LuaEngine.cs
@@ -1,6 +1,6 @@
-﻿using Lua;
-using Lua.Standard;
+﻿using MoonSharp.Interpreter;
 using System.ComponentModel;
+using System.Reflection;
 using System.Text;
 using Tuat.Interfaces;
 using Tuat.Models;
@@ -12,32 +12,35 @@ namespace Tuat.ScriptingEngineLua;
 [Editor("Tuat.ScriptingEngineLua.Editor", typeof(Editor))]
 public class LuaEngine : IScriptEngine
 {
-    private Lua.LuaState? _engine;
+    private Script? _engine;
     private IClientService? _clientService;
     private IDataService? _dataService;
     private IVariableService? _variableService;
     private IAutomationHandler? _automationHandler;
     private List<string> _systemMethods = [];
-    private LuaValue[]? _engineState;
  
+    static LuaEngine()
+    {
+        UserData.RegisterAssembly(Assembly.GetAssembly(typeof(LuaEngine)));
+    }
+
     public void Initialize(IClientService clientService, IDataService dataService, IVariableService variableService, IAutomationHandler automationHandler, Guid instanceId, string? additionalScript, List<AutomationInputVariable>? inputValues = null, int? topAutomationId = null)
     {
         _clientService = clientService;
         _dataService = dataService;
         _variableService = variableService;
         _automationHandler = automationHandler;
-        _engine = LuaState.Create();
-        _engine.OpenStandardLibraries();
+        _engine = new Script();
         var systemMethods = new SystemMethods(_engine, _clientService, _dataService, _variableService, _automationHandler, topAutomationId);
+        _engine.Options.DebugPrint = s => { systemMethods.log(instanceId.ToString(), s); };
+        _engine.Globals["system"] = systemMethods;
         var systemScript = new StringBuilder();
         systemScript.AppendLine(GetSystemScript(_clientService, instanceId, subAutomationParameters: automationHandler.Automation.SubAutomationParameters, inputValues: inputValues));
         if (!string.IsNullOrWhiteSpace(additionalScript))
         {
             systemScript.AppendLine(additionalScript);
         }
-        systemScript.Append("return ");
-        systemScript.AppendLine(string.Join(",", _systemMethods));
-        _engineState = _engine.DoStringAsync(systemScript.ToString()).Result;
+         _engine.DoString(systemScript.ToString());
     }
 
     public string GetReturnTrueStatement()
@@ -68,27 +71,40 @@ public class LuaEngine : IScriptEngine
 
     public List<IScriptEngine.ScriptVariable> GetScriptVariables()
     {
-         return _engine?.Environment.Skip(35)
-            .Where(x => x.Value.GetType() != typeof(LuaFunction) && !x.Value.ToString().StartsWith("function: "))
-            .Select(x => new IScriptEngine.ScriptVariable(x.Key.ToString(), x.Value.ToObject()))
-            .ToList() ?? [];
+        return _engine?.Globals.Pairs
+          .Where(x => x.Value.Type != DataType.ClrFunction 
+            && x.Value.Type != DataType.Function 
+            && x.Value.Type != DataType.UserData 
+            && x.Value.Type != DataType.Table 
+            && x.Key.ToString() != "\"_VERSION\"")
+          .Select(x => new IScriptEngine.ScriptVariable(x.Key.String, x.Value.ToObject()))
+          .ToList() ?? [];
     }
 
     public void CallVoidFunction(string functionName, List<IScriptEngine.FunctionParameter>? functionParameters = null)
     {
-        var index = _systemMethods.IndexOf(functionName);
-        var func = _engineState![index].Read<LuaFunction>();
-        //func.InvokeAsync(_engineState, []);
-        _engine!.CallAsync(func, []);
+        if (functionParameters?.Any()!=true)
+        {
+            _engine!.Call(_engine.Globals[functionName]);
+        }
+        else
+        {
+            _engine!.Call(_engine.Globals[functionName], functionParameters.Select(x => x.Value).ToArray());
+        }
     }
 
     public T CallFunction<T>(string functionName, List<FunctionParameter>? functionParameters = null)
     {
-        var index = _systemMethods.IndexOf(functionName);
-        var func = _engineState![index].Read<LuaFunction>();
-        //func.InvokeAsync(_engineState, []);
-        var luaValue = _engine!.CallAsync(func, []).Result[0];
-        return luaValue.Read<T>();
+        if (functionParameters?.Any() != true)
+        {
+            var result = _engine!.Call(_engine.Globals[functionName]);
+            return (T)result.ToObject();
+        }
+        else
+        {
+            var result = _engine!.Call(_engine.Globals[functionName], functionParameters.Select(x => x.Value).ToArray());
+            return (T)result.ToObject();
+        }
     }
 
     public void Execute(string script)
@@ -103,7 +119,6 @@ public class LuaEngine : IScriptEngine
 
     public void Dispose()
     {
-        _engine?.Dispose();
         _engine = null;
     }
 
@@ -111,7 +126,7 @@ public class LuaEngine : IScriptEngine
     {
         _systemMethods.Add(functionName);
         var result = new StringBuilder();
-        result.Append($"local function {functionName}(");
+        result.Append($"function {functionName}(");
         if (functionParameters?.Any() == true)
         {
             string.Join(", ", functionParameters.Select(p => p.Name));
@@ -135,6 +150,73 @@ public class LuaEngine : IScriptEngine
         script.AppendLine(SystemMethods.SystemScript());
         script.AppendLine();
 
+        var clients = clientService.GetClients();
+        List<string> clientTypesHandled = [];
+        foreach (var client in clients)
+        {
+            if (clientTypesHandled.Contains(client.Client.ClientType))
+            {
+                continue;
+            }
+
+            var createVariableMethods = client.CreateVariableOnClientMethods();
+            var createExecuteMethods = client.CreateExecuteOnClientMethods();
+            if (createVariableMethods.Any() || createExecuteMethods.Any())
+            {
+                script.AppendLine("--===================================================");
+                script.AppendLine($"-- client helper methods for {client.Client.Name}");
+                script.AppendLine("--===================================================");
+
+                foreach (var method in createVariableMethods)
+                {
+                    script.AppendLine($"""--{method.description}""");
+                    script.AppendLine($"""--{method.example}""");
+                    script.AppendLine($"""function {method.methodName}(name, data, mockingOptions, clientId)""");
+                    script.AppendLine($"  if (clientId == nil) then");
+                    script.AppendLine($"    clientId = {client.Client.Id}");
+                    script.AppendLine($"  end");
+                    script.AppendLine($"   return createVariableOnClient(name, clientId, {(method.isAutomationVariable ? "true" : "false")},  {(method.persistant ? "true" : "false")}, data, mockingOptions)");
+                    script.AppendLine("end");
+                }
+
+                foreach (var method in createExecuteMethods)
+                {
+                    script.AppendLine($"""--{method.description}""");
+                    script.AppendLine($"""--{method.example}""");
+                    script.AppendLine($"""function {method.methodName}(variableId, parameter1, parameter2, parameter3, clientId)""");
+                    script.AppendLine($"  if (clientId == nil) then");
+                    script.AppendLine($"    clientId = {client.Client.Id}");
+                    script.AppendLine($"  end");
+                    script.AppendLine($"   return executeOnClient(clientId, variableId, \"{method.command}\", parameter1, parameter2, parameter3)");
+                    script.AppendLine("end");
+                }
+            }
+        }
+
+        script.AppendLine();
+        subAutomationParameters?.ForEach(p =>
+        {
+            var inputVar = inputValues?.FirstOrDefault(x => string.Compare(x.Name, p.Name, true) == 0);
+            if (inputVar != null)
+            {
+                if (inputVar.Value == null)
+                {
+                    script.AppendLine($"{p.ScriptVariableName} = nil");
+                }
+                else if (inputVar.Value is string v)
+                {
+                    script.AppendLine($"{p.ScriptVariableName} = \"{inputVar.Value}\"");
+                }
+                else
+                {
+                    script.AppendLine($"{p.ScriptVariableName} = {inputVar.Value!.ToString()?.Replace(",", ".")}");
+                }
+            }
+            else
+            {
+                script.AppendLine($"{p.ScriptVariableName} = {p.DefaultValue}");
+            }
+        });
         return script.ToString();
     }
 }
--- src/Tuat.ScriptingEngineLua/SystemMethods.cs
+++ src/Tuat.ScriptingEngineLua/SystemMethods.cs
@@ -1,4 +1,4 @@
-﻿using Lua;
+﻿using MoonSharp.Interpreter;
 using System.Collections.Concurrent;
 using System.Dynamic;
 using System.Text;
@@ -7,6 +7,7 @@ using Tuat.Models;
 
 namespace Tuat.ScriptingEngineLua;
 
+[MoonSharpUserData]
 internal class SystemMethods
 {
     private readonly IVariableService _variableService;
@@ -16,9 +17,10 @@ internal class SystemMethods
     private readonly ConcurrentDictionary<int, Client> _clients;
     private readonly int? _topAutomationId;
 
+    [MoonSharpUserData]
     public record DateTimeInfo(int year, int month, int day, int hour, int minute, int second, int dayOfWeek);
 
-    public SystemMethods(LuaState engine, IClientService clientService, IDataService dataService, IVariableService variableService, IAutomationHandler automationHandler, int? topAutomationId)
+    public SystemMethods(Script engine, IClientService clientService, IDataService dataService, IVariableService variableService, IAutomationHandler automationHandler, int? topAutomationId)
     {
         _variableService = variableService;
         _automationHandler = automationHandler;
@@ -26,124 +28,121 @@ internal class SystemMethods
         _dataService = dataService;
         _clients = new ConcurrentDictionary<int, Client>(dataService.GetClients().ToDictionary(c => c.Id));
         _topAutomationId = topAutomationId;
-
-        engine.Environment["system_log"] = new LuaFunction("system_log", async (context, ct) =>
-        {
-            var instanceId = context.GetArgument<string>(0);
-            var message = context.GetArgument<string>(1);
-            await _automationHandler.AddLogAsync(instanceId, message);
-            return 1;
-        });
     }
 
-    //public DateTimeInfo getCurrentDateTime()
-    //{
-    //    var now = DateTime.Now;
-    //    return new DateTimeInfo(now.Year, now.Month, now.Day, now.Hour, now.Minute, now.Second, (int)now.DayOfWeek);
-    //}
+    public void log(string instanceId, string message)
+    {
+        _automationHandler.AddLogAsync(instanceId, message);
+    }
 
-    //public bool currentTimeBetween(string startTime, string endTime, bool includeBoundary)
-    //{
-    //    var currentTime = DateTime.Now.TimeOfDay;
-    //    return currentTime.TimeOfDayBetween(startTime, endTime, includeBoundary);
-    //}
+    public DateTimeInfo getCurrentDateTime()
+    {
+        var now = DateTime.Now;
+        return new DateTimeInfo(now.Year, now.Month, now.Day, now.Hour, now.Minute, now.Second, (int)now.DayOfWeek);
+    }
 
-    //public int createVariable(string name, int clientId, bool isAutomationVariable, bool persistant, JsValue? data, JsValue[]? mockingOptions)
-    //{
-    //    List<string>? stringMockingOptions = null;
-    //    if (mockingOptions?.Any() == true)
-    //    {
-    //        stringMockingOptions = [];
-    //        foreach (var mockingOption in mockingOptions)
-    //        {
-    //            stringMockingOptions.Add(mockingOption.JsValueToString(false));
-    //        }
-    //    }
-    //    return _variableService.CreateVariableAsync(name, clientId, isAutomationVariable ? (_topAutomationId ?? _automationHandler.Automation.Id) : null, persistant, data?.ToString(), stringMockingOptions).Result ?? -1;
-    //}
+    public bool currentTimeBetween(string startTime, string endTime, bool includeBoundary)
+    {
+        var currentTime = DateTime.Now.TimeOfDay;
+        return currentTime.TimeOfDayBetween(startTime, endTime, includeBoundary);
+    }
 
-    //public int? getVariableIdByName(string name, int clientId, bool isStateMachineVariable)
-    //{
-    //    var variable = _variableService
-    //            .GetVariables()
-    //            .FirstOrDefault(v => v.Variable.Name == name
-    //                                && v.Variable.ClientId == clientId
-    //                                && (isStateMachineVariable && v.Variable.AutomationId == (_topAutomationId ?? _automationHandler.Automation.Id) || !isStateMachineVariable && v.Variable.AutomationId == null));
-    //    return variable?.Variable.Id;
-    //}
+    public int createVariable(string name, int clientId, bool isAutomationVariable, bool persistant, object? data, object[]? mockingOptions)
+    {
+        List<string>? stringMockingOptions = null;
+        if (mockingOptions?.Any() == true)
+        {
+            stringMockingOptions = [];
+            foreach (var mockingOption in mockingOptions)
+            {
+                stringMockingOptions.Add(mockingOption.ToString()!);
+            }
+        }
+        return _variableService.CreateVariableAsync(name, clientId, isAutomationVariable ? (_topAutomationId ?? _automationHandler.Automation.Id) : null, persistant, data?.ToString(), stringMockingOptions).Result ?? -1;
+    }
 
-    //public bool setVariableValue(int variableId, string? value)
-    //{
-    //    return _variableService.SetVariableValuesAsync([(variableId, value)]).Result;
-    //}
+    public int? getVariableIdByName(string name, int clientId, bool isStateMachineVariable)
+    {
+        var variable = _variableService
+                .GetVariables()
+                .FirstOrDefault(v => v.Variable.Name == name
+                                    && v.Variable.ClientId == clientId
+                                    && (isStateMachineVariable && v.Variable.AutomationId == (_topAutomationId ?? _automationHandler.Automation.Id) || !isStateMachineVariable && v.Variable.AutomationId == null));
+        return variable?.Variable.Id;
+    }
 
-    //public string? getVariableValue(int variableId)
-    //{
-    //    return _variableService.GetVariable(variableId)?.Value;
-    //}
+    public bool setVariableValue(int variableId, string? value)
+    {
+        return _variableService.SetVariableValuesAsync([(variableId, value)]).Result;
+    }
 
-    //public bool isMockingVariableActive(int variableId)
-    //{
-    //    return _variableService.GetVariable(variableId)?.IsMocking ?? false;
-    //}
+    public string? getVariableValue(int variableId)
+    {
+        return _variableService.GetVariable(variableId)?.Value;
+    }
 
-    //public int getClientId(string name)
-    //{
-    //    var client = _clients.Values.FirstOrDefault(c => c.Name == name);
-    //    return client?.Id ?? -1;
-    //}
+    public bool isMockingVariableActive(int variableId)
+    {
+        return _variableService.GetVariable(variableId)?.IsMocking ?? false;
+    }
 
-    //public int getAutomationId(string name)
-    //{
-    //    var automation = _dataService.GetAutomations().FirstOrDefault(c => c.Name == name);
-    //    return automation?.Id ?? -1;
-    //}
+    public int getClientId(string name)
+    {
+        var client = _clients.Values.FirstOrDefault(c => c.Name == name);
+        return client?.Id ?? -1;
+    }
 
-    //public bool clientExecute(int clientId, int? variableId, string command, object? parameter1, object? parameter2, object? parameter3)
-    //{
-    //    return _clientService.ExecuteAsync(clientId, variableId, command, parameter1, parameter2, parameter3).Result;
-    //}
+    public int getAutomationId(string name)
+    {
+        var automation = _dataService.GetAutomations().FirstOrDefault(c => c.Name == name);
+        return automation?.Id ?? -1;
+    }
 
-    //public void setAutomationFinished(object? scriptOutputValues)
-    //{
-    //    List<AutomationOutputVariable> outputValues = [];
-    //    if (scriptOutputValues != null)
-    //    {
-    //        var array = (object[])scriptOutputValues;
-    //        foreach (ExpandoObject outputValue in array)
-    //        {
-    //            var name = outputValue.First().Key;
-    //            var value = outputValue.First().Value;
-    //            outputValues.Add(new AutomationOutputVariable { Name = name, Value = value });
-    //        }
-    //    }
-    //    _automationHandler.SetAutomationFinished(outputValues);
-    //}
-    //public void startSubAutomation(int automationId, object? scriptInputValues)
-    //{
-    //    List<AutomationInputVariable> inputValues = [];
-    //    if (scriptInputValues != null)
-    //    {
-    //        var array = (object[])scriptInputValues;
-    //        foreach (ExpandoObject inputValue in array)
-    //        {
-    //            var name = inputValue.First().Key;
-    //            var value = inputValue.First().Value;
-    //            inputValues.Add(new AutomationInputVariable { Name = name, Value = value });
-    //        }
-    //    }
-    //    _automationHandler.StartSubAutomation(automationId, inputValues);
-    //}
+    public bool clientExecute(int clientId, int? variableId, string command, object? parameter1, object? parameter2, object? parameter3)
+    {
+        return _clientService.ExecuteAsync(clientId, variableId, command, parameter1, parameter2, parameter3).Result;
+    }
 
-    //public bool isSubAutomationRunning()
-    //{
-    //    return _automationHandler.IsSubAutomationRunning();
-    //}
+    public void setAutomationFinished(object? scriptOutputValues)
+    {
+        List<AutomationOutputVariable> outputValues = [];
+        if (scriptOutputValues != null)
+        {
+            var array = ((MoonSharp.Interpreter.Table)scriptOutputValues).Pairs;
+            foreach (var outputValue in array)
+            {
+                var name = outputValue.Key.String;
+                var value = outputValue.Value.ToObject();
+                outputValues.Add(new AutomationOutputVariable { Name = name, Value = value });
+            }
+        }
+        _automationHandler.SetAutomationFinished(outputValues);
+    }
+    public void startSubAutomation(int automationId, object? scriptInputValues)
+    {
+        List<AutomationInputVariable> inputValues = [];
+        if (scriptInputValues != null)
+        {
+            var array = ((MoonSharp.Interpreter.Table)scriptInputValues).Pairs;
+            foreach (var inputValue in array)
+            {
+                var name = inputValue.Key.String;
+                var value = inputValue.Value.ToObject();
+                inputValues.Add(new AutomationInputVariable { Name = name, Value = value });
+            }
+        }
+        _automationHandler.StartSubAutomation(automationId, inputValues);
+    }
 
-    //public void stopSubAutomation()
-    //{
-    //    _automationHandler.StopSubAutomation();
-    //}
+    public bool isSubAutomationRunning()
+    {
+        return _automationHandler.IsSubAutomationRunning();
+    }
+
+    public void stopSubAutomation()
+    {
+        _automationHandler.StopSubAutomation();
+    }
 
     public static string SystemScript()
     {
@@ -158,87 +157,86 @@ internal class SystemMethods
     -- SYSTEM METHODS
     --====================================================================================
     
-    local function log(message)
-       system_log(instanceId, message)
+    function log(message)
+       system.log(instanceId, message)
     end
 
-    --[[
-    ////returns the current (local) date and time as an object with year, month, day, hour, minute, second and dayOfWeek properties
-    //getCurrentDateTime = function() {
-    //    return system.getCurrentDateTime()
-    //}
-
-    //currentTimeBetween = function(startTime, endTime, includeBoundary) {
-    //    //e.g. currentTimeBetween('8:00', '18:00', true)
-    //    // currentTimeBetween('22:00', '4:00', true)
-    //    // currentTimeBetween('22:00', '0:30', true)
-    //    //it used the 24h clock format without leading zeros
-    //    //returns true if the current time is between startTime and endTime (inclusive or exclusive depending on includeBoundary)
-    //    return system.currentTimeBetween(startTime, endTime, includeBoundary)
-    //}
     
-    //// returns the client id or -1 if not found
-    //getClientId = function(name) {
-    //    return system.getClientId(name)
-    //}
+    --returns the current (local) date and time as an object with year, month, day, hour, minute, second and dayOfWeek properties
+    function getCurrentDateTime()
+        return system.getCurrentDateTime()
+    end
 
-    //// returns the automation id or -1 if not found
-    //getAutomationId = function(name) 
-    //{
-    //    return system.getAutomationId(name)
-    //}
-
-    ////execute specific client commands (-1 if it fails)
-    ////e.g. executeOnClient(clientIdOfHomeAssistant, null, 'callservice', 'light', 'turn_on', { "entity_id": "light.my_light", "brightness_pct": 20})
-    //executeOnClient = function(clientId, variableId, command, parameter1, parameter2, parameter3) {
-    //    return system.clientExecute(clientId, variableId, command, parameter1, parameter2, parameter3)
-    //}
-
-    //// creates a variable and returns the variable id (-1 if it fails)
-    //// e.g. createVariable('test', clientId, true, true, 'initialValue', ['option1', 'option2'])
-    //createVariableOnClient = function(name, clientId, isAutomationVariable, persistant, data, mockingOptions) {
-    //    return system.createVariable(name, clientId, isAutomationVariable, persistant, data, mockingOptions)
-    //}
-
-    //// returns the variable value
-    //getVariableValue = function(variableId) {
-    //    return system.getVariableValue(variableId)
-    //}
-
-    //// sets the variable value (returns true if successful, false otherwise)
-    //setVariableValue = function(variableId, variableValue) {
-    //    return system.setVariableValue(variableId, variableValue)
-    //}
-
-    ////get the variable Id by name
-    //getVariableIdByName = function(name, clientId, isAutomationVariable) {
-    //    return system.getVariableIdByName(name, clientId, isAutomationVariable)
-    //}
-
-    //isMockingVariableActive = function(variableId) {
-    //    return system.isMockingVariableActive(variableId)
-    //}
+    function currentTimeBetween(startTime, endTime, includeBoundary)
+        --e.g. currentTimeBetween('8:00', '18:00', true)
+        -- currentTimeBetween('22:00', '4:00', true)
+        -- currentTimeBetween('22:00', '0:30', true)
+        --it used the 24h clock format without leading zeros
+        --returns true if the current time is between startTime and endTime (inclusive or exclusive depending on includeBoundary)
+        return system.currentTimeBetween(startTime, endTime, includeBoundary)
+    end
     
-    //// start a sub automation and provide input values (array of { 'name': value })
-    //startSubAutomation = function(automationId, scriptInputValues) {
-    //    system.startSubAutomation(automationId, scriptInputValues)
-    //}
+    -- returns the client id or -1 if not found
+    function getClientId(name)
+        return system.getClientId(name)
+    end
 
-    //// check if the started sub-automation is still running
-    //isSubAutomationRunning = function() {
-    //    return system.isSubAutomationRunning()
-    //}
+    -- returns the automation id or -1 if not found
+    function getAutomationId(name) 
+        return system.getAutomationId(name)
+    end
 
-    //// set the automation as finished and provide output values (array of { 'name': value })
-    //setAutomationFinished = function(outputValues) {
-    //    return system.setAutomationFinished(outputValues)
-    //}
+    --execute specific client commands (-1 if it fails)
+    --e.g. executeOnClient(clientIdOfHomeAssistant, null, "callservice", "light", "turn_on", { "entity_id": "light.my_light", "brightness_pct": 20})
+    function executeOnClient(clientId, variableId, command, parameter1, parameter2, parameter3)
+        return system.clientExecute(clientId, variableId, command, parameter1, parameter2, parameter3)
+    end
+
+    -- creates a variable and returns the variable id (-1 if it fails)
+    -- e.g. createVariable('test', clientId, true, true, "initialValue", {"option1", "option2"})
+    function createVariableOnClient(name, clientId, isAutomationVariable, persistant, data, mockingOptions)
+        return system.createVariable(name, clientId, isAutomationVariable, persistant, data, mockingOptions)
+    end
+
+    -- returns the variable value
+    function getVariableValue(variableId)
+        return system.getVariableValue(variableId)
+    end
+
+    -- sets the variable value (returns true if successful, false otherwise)
+    function setVariableValue(variableId, variableValue)
+        return system.setVariableValue(variableId, variableValue)
+    end
+
+    --get the variable Id by name
+    function getVariableIdByName(name, clientId, isAutomationVariable)
+        return system.getVariableIdByName(name, clientId, isAutomationVariable)
+    end
+
+    function isMockingVariableActive(variableId) 
+        return system.isMockingVariableActive(variableId)
+    end
     
-    //// stop the running sub-automation
-    //stopSubAutomation = function() {
-    //    return system.stopSubAutomation()
-    //}
-    ]]
+    -- start a sub automation and provide input values (Table of { "name": value })
+    function startSubAutomation(automationId, scriptInputValues)
+        system.startSubAutomation(automationId, scriptInputValues)
+    end
+
+    -- check if the started sub-automation is still running
+    function isSubAutomationRunning()
+        return system.isSubAutomationRunning()
+    end
+
+    -- set the automation as finished and provide output values (Table of { "name": value })
+    function setAutomationFinished(outputValues)
+        return system.setAutomationFinished(outputValues)
+    end
+    
+    -- stop the running sub-automation
+    function stopSubAutomation()
+        return system.stopSubAutomation()
+    end
+    
     """";
 }
 
